f2776911924af12c86394e207ba9873475ce058b
refactorized <adam.tolley+git@gmail.com>
2024-06-30 09:18:19 -0400

    simple site build

---
+++ src/build.js
@@ -0,0 +1,34 @@
+import puppeteer from 'puppeteer' // or import puppeteer from 'puppeteer-core';
+import Eleventy from '@11ty/eleventy'
+import { mkdirp } from 'mkdirp'
+
+const port = process?.env?.PORT || 3927
+const path = process?.env?.OUT_DIR || './_site/resume/resume.pdf'
+const pathDir = path.split('/').slice(0, -1).join('/')
+
+await mkdirp(pathDir)
+
+const elly = new Eleventy()
+await elly.init()
+await elly.write()
+elly.serve(port)
+
+// Launch the browser and open a new blank page
+const browser = await puppeteer.launch()
+const page = await browser.newPage()
+
+// todo: pre-build and serve
+// Set screen size.
+await page.setViewport({ width: 1080, height: 1024 })
+
+await page.goto(`http://localhost:${port}/`, {
+  waitUntil: 'networkidle2',
+})
+
+await page.pdf({
+  path,
+})
+
+await browser.close()
+
+process.exit(0)
